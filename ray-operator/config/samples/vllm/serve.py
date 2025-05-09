65e3d62dd1bd119e81ac0af2b5e9378af8651d38
Yen Kai Lim <yenlim@cisco.com>
2025-05-09 14:54:51 +0800

    added distributed executor backend argument

--- ray-operator/config/samples/vllm/serve.py
+++ ray-operator/config/samples/vllm/serve.py
@@ -133,6 +133,7 @@ def build_app(cli_args: Dict[str, str]) -> serve.Application:
 model = build_app({
     "model": os.environ['MODEL_ID'],
     "tensor-parallel-size": os.environ['TENSOR_PARALLELISM'],
-    "pipeline-parallel-size": os.environ['PIPELINE_PARALLELISM']
+    "pipeline-parallel-size": os.environ['PIPELINE_PARALLELISM'],
+    "distributed_executor_backend": os.environ['DISTRIBUTED_EXECUTOR_BACKEND']
 })
 
